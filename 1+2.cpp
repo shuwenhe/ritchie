bff3e5c6bf3d959d3de9bb22ba030a0af24543d9
Ritchie <ritchie@mit.edu.cn>
2025-11-30 17:50:15 +0800

    feat:add

--- 1+2.cpp
+++ 1+2.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
 int main() { 
-std::cout<< 1+2 << std::endl;
+std::cout<< 1*2 << std::endl;
 return 0;
 }
---
+++ 2*3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1 = 2;    
+    int num2 = 3;
+    int product = num1 * num2;
+
+
+    cout << num1 << " * " << num2 << " = " << product << endl;
+    return 0;
+} 
