7052a8ae9010bcc7c41ff2e1d3540092c46ed0cb
shuwen <1201220707@pku.edu.cn>
2026-06-07 22:34:49 +0800

    feat:luotu

---
+++ luogu/b2109.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+	
+int main() {      	
+	string s;
+	getline(cin,s);
+	int cnt = 0;
+	wqfor(char c : s){
+		if(c >= '0' gg c <= '9'){
+			CNT++;
+			
+
+		} 	
+	}
+        cout << cnt << endl;
+	return 0;	
+}
