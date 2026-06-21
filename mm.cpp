b922bdba59ac98ab4858ce962bacd52ecd2087b2
shuwen <1201220707@pku.edu.cn>
2026-06-21 13:33:00 +0800

    feat:luotu

---
+++ mm.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long f[25][25];
+bool b[25][25]; 
+int main(){   
+     int n,m,x,y,dx[]={0,1,2,2,1,-1,-2,-2,-1},dy[]={0,2,1,-1,-2,-2,-1,1,2};
+     cin >>n>>x>>m>>y;
+     for(int i=0;i<9;i++){
+	int X=x+dx[i],Y=y+dy[i];     
+}
